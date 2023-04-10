dd34091a4184f88d6b7ed85378b0b1e97456df7a
Hwang Seung Hyeon <70440577+ggg06139@users.noreply.github.com>
2023-04-10 20:56:09 +0900

    Fix: "누락된 주석 추가"

--- line_detection/region_of_interest.py
+++ line_detection/region_of_interest.py
@@ -1,3 +1,9 @@
+"""
+작성자 정보: 황승현 / 전남대학교 전자컴퓨터공학부 전자정보통신공학전공 / ggg06139@gmail.com
+코드 기능: ROI(Region of Interest)를 설정하는 함수가 구현되어 있습니다.
+최종 수정 시간: 2023년 4월 10일 20:56
+"""
+
 import cv2
 import numpy as np
 
